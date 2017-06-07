2642cadbe44fe273ed817bb6cdf5ab46da6f1dde
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2017-06-06 20:57:52 -0700

    Snow uses subsurface scattering, parameters set to sugar

--- src/materials/snow.h
+++ src/materials/snow.h
@@ -43,36 +43,87 @@
 #include "material.h"
 
 namespace pbrt {
+// SubsurfaceMaterial Declarations
+class SubsurfaceMaterial : public Material {
+  public:
+    // SubsurfaceMaterial Public Methods
+    SubsurfaceMaterial(Float scale,
+                       const std::shared_ptr<Texture<Spectrum>> &Kr,
+                       const std::shared_ptr<Texture<Spectrum>> &Kt,
+                       const std::shared_ptr<Texture<Spectrum>> &sigma_a,
+                       const std::shared_ptr<Texture<Spectrum>> &sigma_s,
+                       Float g, Float eta,
+                       const std::shared_ptr<Texture<Float>> &uRoughness,
+                       const std::shared_ptr<Texture<Float>> &vRoughness,
+                       const std::shared_ptr<Texture<Float>> &bumpMap,
+                       bool remapRoughness)
+        : scale(scale),
+          Kr(Kr),
+          Kt(Kt),
+          sigma_a(sigma_a),
+          sigma_s(sigma_s),
+          uRoughness(uRoughness),
+          vRoughness(vRoughness),
+          bumpMap(bumpMap),
+          eta(eta),
+          remapRoughness(remapRoughness),
+          table(100, 64) {
+        ComputeBeamDiffusionBSSRDF(g, eta, &table);
+    }
+    void ComputeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena,
+                                    TransportMode mode,
+                                    bool allowMultipleLobes) const;
 
+  private:
+    // SubsurfaceMaterial Private Data
+    const Float scale;
+    std::shared_ptr<Texture<Spectrum>> Kr, Kt, sigma_a, sigma_s;
+    std::shared_ptr<Texture<Float>> uRoughness, vRoughness;
+    std::shared_ptr<Texture<Float>> bumpMap;
+    const Float eta;
+    const bool remapRoughness;
+    BSSRDFTable table;
+};
 // SnowMaterial Declarations
 class SnowMaterial : public Material {
   public:
     // SnowMaterial Public Methods
-    SnowMaterial(const std::shared_ptr<Texture<Spectrum>> &Kr,
+    SnowMaterial(Float scale,
+                  const std::shared_ptr<Texture<Spectrum>> &Kr,
                   const std::shared_ptr<Texture<Spectrum>> &Kt,
+                  const std::shared_ptr<Texture<Spectrum>> &sigma_a,
+                  const std::shared_ptr<Texture<Spectrum>> &sigma_s,
+                  Float g, Float eta,
                   const std::shared_ptr<Texture<Float>> &uRoughness,
                   const std::shared_ptr<Texture<Float>> &vRoughness,
-                  const std::shared_ptr<Texture<Float>> &index,
                   const std::shared_ptr<Texture<Float>> &bumpMap,
                   bool remapRoughness)
-        : Kr(Kr),
+        : scale(scale),
+          Kr(Kr),
           Kt(Kt),
+          sigma_a(sigma_a),
+          sigma_s(sigma_s),
           uRoughness(uRoughness),
           vRoughness(vRoughness),
-          index(index),
           bumpMap(bumpMap),
-          remapRoughness(remapRoughness) {}
+          eta(eta),
+          remapRoughness(remapRoughness),
+          table(100, 64) {
+        ComputeBeamDiffusionBSSRDF(g, eta, &table)
+    }
     void ComputeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena,
                                     TransportMode mode,
                                     bool allowMultipleLobes) const;
 
   private:
     // SnowMaterial Private Data
-    std::shared_ptr<Texture<Spectrum>> Kr, Kt;
+    const Float scale;
+    std::shared_ptr<Texture<Spectrum>> Kr, Kt, sigma_a, sigma_s;
     std::shared_ptr<Texture<Float>> uRoughness, vRoughness;
-    std::shared_ptr<Texture<Float>> index;
     std::shared_ptr<Texture<Float>> bumpMap;
+    const Float eta;
     bool remapRoughness;
+    BSSDRFTable table;
 };
 
 SnowMaterial *CreateSnowMaterial(const TextureParams &mp);
